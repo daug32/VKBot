c4e2523b7084ed206434aaa8ec43df56dba60527
Dmitry <73381002+daug32@users.noreply.github.com>
2022-02-02 12:53:11 +0300

    Update config.php

--- config.php
+++ config.php
@@ -2,9 +2,9 @@
 
 define("VK_API_END_POINT", "https://api.vk.com/method/");
 
-define("VK_API_ACCESS_TOKEN", "5a2bbc6b2710334b9cd27269792cfa8c17d213e59bce0fa522178b08f1f1cbb7bb3417dafa1dc06414dc0");
-define("VK_API_AUTHOR_ID", 459310271);
-define("VK_API_GROUP_ID", -210078971);
+define("VK_API_ACCESS_TOKEN", "___");
+define("VK_API_AUTHOR_ID", "___");
+define("VK_API_GROUP_ID", "___");
 define("VK_API_VERSION", "5.130");
 
 ?>
